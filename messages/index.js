c9f37c8ee8bca0d7fb591e81ab4a91aed3839397
Menashe Gezelkopy <menashe.gezelkopy@gmail.com>
2017-04-11 23:30:45 +0300

    reminder_ver_08

--- messages/index.js
+++ messages/index.js
@@ -508,6 +508,10 @@ bot.dialog('/createReminder', [
 
                 //new Date(Date.UTC(year, month, day, hour, minute, second))
 
+                var offset = new Date().getTimezoneOffset();
+
+                session.send("offset: " + offset);
+
                 
                 var date = new Date(Date.UTC(ReminderYear, session.userData.ReminderMonth, session.userData.ReminderDay, session.userData.ReminderTime, minutes, 0));
 
